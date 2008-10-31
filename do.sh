627db35dbbfa7478319f93022dc32176dbf41299
malc <av1474@comtv.ru>
2008-10-31 23:28:07 +0300

    Add -O to compilation options

--- do.sh
+++ do.sh
@@ -1,4 +1,4 @@
-ocamlc -o dormin -custom -I +lablGL unix.cma lablgl.cma lablglut.cma \
+ocamlc -ccopt -O -o dormin -custom -I +lablGL unix.cma lablgl.cma lablglut.cma \
  skin.c swizzle.c \
  slice.ml vec.ml qtr.ml xff.ml rend.ml skin.ml anb.ml skb.ml nto.ml nmo.ml
 
