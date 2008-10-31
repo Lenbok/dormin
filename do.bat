84b28df06cef3ac91ba55cc68d0d13a285944fed
malc <av1474@comtv.ru>
2008-10-31 23:27:52 +0300

    Add -O2 to compilation options

--- do.bat
+++ do.bat
@@ -1,1 +1,1 @@
-ocamlc -o dormin -custom -I +lablGL unix.cma lablgl.cma lablglut.cma skin.c swizzle.c slice.ml vec.ml qtr.ml xff.ml rend.ml skin.ml anb.ml skb.ml nto.ml nmo.ml
+ocamlc -ccopt -O2 -o dormin -custom -I +lablGL unix.cma lablgl.cma lablglut.cma skin.c swizzle.c slice.ml vec.ml qtr.ml xff.ml rend.ml skin.ml anb.ml skb.ml nto.ml nmo.ml
