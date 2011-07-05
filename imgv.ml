fe1e93cc89a3b7f2a3dfc9419a1971eed6690cfe
malc <av1474@comtv.ru>
2011-07-05 16:12:58 +0400

    Drop swap and adopt to changes to nto.ml

--- imgv.ml
+++ imgv.ml
@@ -60,13 +60,11 @@ let display () =
 let main =
   let level = ref 0 in
   let nto_name = ref None in
-  let swap = ref false in
   let setsome r s = r := Some s in
   let spec =
     ["-index", Arg.Set_string Xff.index_path, " <path> to index"
     ;"-base", Arg.String (setsome Xff.base_path), " <directory> base"
     ;"-level", Arg.Set_int level, " <level> mipmap level"
-    ;"-swap", Arg.Set swap, " swap width and height"
     ]
   in
   Arg.parse (Arg.align spec) (setsome nto_name) "Usage: imgv image.nto";
@@ -76,9 +74,8 @@ let main =
     | Some s -> Filename.basename s
   in
   let xff, sbuf = Xff.test2 nto_name in
-  let nto = Nto.r xff sbuf () in
+  let nto = Nto.r xff sbuf in
   let image2d level (w, h, data) =
-    let w, h = if !swap then (h, w) else (w, h) in
     let id = GlTex.gen_texture () in
     Gl.enable `texture_2d;
     GlTex.bind_texture `texture_2d id;
@@ -104,7 +101,6 @@ let main =
       !level
   in
   let (w, h, img) as nto = nto.(level) in
-  let w, h = if !swap then (h, w) else (w, h) in
   view.w <- w;
   view.h <- h;
   view.tw <- float w;
